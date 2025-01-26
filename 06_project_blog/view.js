5e2fc2ee42a434aaabdbeeda3ccf96f078195a5f
SSR <iamssrobinson@gmail.com>
2025-01-26 14:56:35 +0000

    Font.
    Added default font-family and system font stack as fallback for the project. Handled inputs, placeholders and buttons.

--- 06_project_blog/view.js
+++ 06_project_blog/view.js
@@ -1,16 +1,19 @@
 import Header from './components/Header/index.js';
 import Logo from './components/Logo/index.js';
+import SubscribeForm from './components/SubscribeForm/index.js';
 
 const view = {
     init : function () {
         this.body = document.body;
         this.bodyContainer = document.querySelector(".body-container");
         this.header = Header();
+        this.subscribeForm = SubscribeForm();
         this.bodyContainer.append(this.header);
 
         this.headerWrapper = document.querySelector(".header-wrapper");
         this.logo = Logo();
         this.headerWrapper.append(this.logo);
+        this.headerWrapper.append(this.subscribeForm);
     }
 };
 
