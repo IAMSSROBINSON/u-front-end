6640512f02df792954317cc2a7f602187e6858b7
SSR <iamssrobinson@gmail.com>
2025-01-30 16:06:42 +0000

    Add design mockups to assets.

--- 06_project_blog/src/view.js
+++ 06_project_blog/src/view.js
@@ -1,4 +1,14 @@
 const view = {
+    init : function () {
+        // cache the DOM
+        this.body = document.body;
+        this.header = document.querySelector(".header");
+        this.mainHeading =  document.querySelector(".main-heading");
+        this.subjectsList =  document.querySelector(".subjects-list");
+        this.main = document.querySelector(".main");
+        this.footer = document.querySelector(".footer");
+  
+    }
 
 }
 export default view;
