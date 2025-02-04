4a0a2e01b3f56e813e221a98584978d2904e21e7
SSR <iamssrobinson@gmail.com>
2025-02-04 14:59:29 +0000

    Menu tracks to homepage for now.

--- 06_project_blog/public/components/Menu/Menu.js
+++ 06_project_blog/public/components/Menu/Menu.js
@@ -2,12 +2,17 @@ function Menu () {
     const container = document.createElement("div");
     container.classList.add("menu-icon-container");
 
+    const anchor = document.createElement("a");
+    anchor.setAttribute("href", "index.html");
+
     const image = document.createElement("img");
     image.classList.add("menu-icon");
     image.src = '/public/assets/icons/menu.svg';
     image.setAttribute("alt", "Hamburger menu navigation icon click for quick links.");
 
-    container.appendChild(image);
+    anchor.appendChild(image);
+
+    container.appendChild(anchor);
     return container;
 }
 export default Menu;
